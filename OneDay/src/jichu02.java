4f1a4332f427e9fe8bfc5572c75a24160a3cfcff
zhengfucong <790909311@qq.com>
2023-05-23 10:15:28 +0800

    基础数据类型学习

---
+++ OneDay/src/jichu02.java
@@ -0,0 +1,21 @@
+public class jichu02 {
+    public static void main(String[] args) {
+        //基本数据类型
+        String a  = "字符串";
+        char b = 'z';//字符
+        int c = 10;//整数
+        Integer d =100;//整数
+
+        byte h =1;//整数
+        Byte o =3;
+        short j = 11;//整数
+        Long e = 10L;//整数
+        float f = 1.0f;//浮点数
+        double g = 3.14;//浮点数
+        boolean k = true;
+        boolean l = false;
+        //引用类型 类 接口 数组
+        //字节 位bit  字节 byte 字符
+        //1bit表示1位  1Byte表示一个字节1B=8b
+    }
+}
