50da7f964720fb07669963a55dca5185e0c57542
zhengfucong <790909311@qq.com>
2023-05-30 20:58:41 +0800

    运算符学习-三元运算符? :

---
+++ OneDay/src/operator/jichu08.java
@@ -0,0 +1,19 @@
+package operator;
+
+public class jichu08 {
+    public static void main(String[] args) {
+        //运算 有() 优先级最高
+        int a = 10;
+        int b = 20;
+        a += b;
+
+        System.out.println(a);
+        System.out.println(a+b+"");
+        //三元运算符
+        //? :   a ? y:x  a是真 返回Y 是假返回x
+        int score = 80;
+//        String type =  score< 60 ? "优秀" : "不合格";
+//        System.out.println(type);
+
+    }
+}
